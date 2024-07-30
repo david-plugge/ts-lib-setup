48d904e3c6600b9af7806b341be5746a1cc0f990
David Plugge <git@davidplugge.de>
2024-07-30 14:27:07 +0200

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default ts.config(
+	js.configs.recommended,
+	...ts.configs.recommendedTypeChecked,
+	...ts.configs.stylisticTypeChecked,
+	prettier,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
+	},
+	{
+		ignores: ['node_modules/', 'build/', 'dist/', '**/.*', 'vitest.config.ts', 'eslint.config.js'],
+	},
+	{
+		rules: {
+			'@typescript-eslint/consistent-type-exports': 'error',
+			'@typescript-eslint/consistent-type-imports': 'error',
+		},
+	},
+);
